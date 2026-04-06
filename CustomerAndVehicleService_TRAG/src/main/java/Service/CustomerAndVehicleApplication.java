6dae7c1dceb448a366cefb61dbb69519af094ad1
SEHUZ <sonic15622@gmail.com>
2026-04-05 22:11:11 -0700

    Borrar cosas del proyecto original y acomodar bien para los microservicios
    DESACOPLAR EL POM PADRE AHORA TODO ESTA POR SEPARADO

---
+++ CustomerAndVehicleService_TRAG/src/main/java/Service/CustomerAndVehicleApplication.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Service;
+
+/**
+ *
+ * @author sonic
+ */
+@SpringBootApplication 
+public class CustomerAndVehicleApplication {
+
+    public static void main(String[] args) {
+        // ¡Arranca el microservicio!
+        SpringApplication.run(CustomerAndVehicleApplication.class, args);
+    }
+    
+}
